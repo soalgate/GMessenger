9d3ccbeb812c24656494f0116e3b42a6f3de0306
Soaltomas <soaltomas@gmail.com>
2018-10-27 15:14:00 +0300

    First commit

---
+++ GMessenger/App/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GMessenger
+//
+//  Created by Артем Полушин on 27.10.2018.
+//  Copyright © 2018 Артем Полушин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
