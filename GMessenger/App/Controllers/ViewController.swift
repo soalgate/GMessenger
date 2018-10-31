47078717c6feba3608fb7777c706db0219e88020
Soaltomas <soaltomas@gmail.com>
2018-10-31 20:02:43 +0300

    Базовое сетевое взаимодействие

--- GMessenger/App/Controllers/ViewController.swift
+++ GMessenger/App/Controllers/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import XMPPFramework
 
 class ViewController: UIViewController {
+    
+    let streamManager = StreamManager()
+    
+    var stream: XMPPStream!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        stream = streamManager.stream(jid: "justeryoda@jabber.mipt.ru",
+                                      password: "^vUttC^%6gHFh5")
+        
+        let button = UIButton()
+        button.backgroundColor = .red
+        button.setTitle("SendMessage", for: .normal)
+        button.frame = CGRect(x: 90, y: 100, width: 300, height: 40)
+        button.addTarget(self, action: #selector(self.sendMessage), for: .touchUpInside)
+        
+        self.view.addSubview(button)
     }
 
-
+    @objc func sendMessage() {
+        let msg = XMPPMessage(type: "chat", to: XMPPJID(string: "justervader@jabber.mipt.ru"))
+        msg.addBody("Test message")
+        stream.send(msg)
+    }
 }
 
